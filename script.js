1a5909680c28f12db6c5c364d46ff4678a35d39c
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-02-19 15:22:42 +0100

    [add] stampato risultato in console

---
+++ script.js
@@ -0,0 +1,13 @@
+const nomeUtente = prompt('Qual è il tuo nome');
+console.log(nomeUtente);
+
+const cognomeUtente = prompt('Qual è il tuo cognome');
+console.log(cognomeUtente);
+
+const colorePreferitoUtente = prompt('Qual è il tuo colore preferito');
+console.log(colorePreferitoUtente);
+
+const numero = 24;
+
+const output = nomeUtente + cognomeUtente + colorePreferitoUtente + numero;
+console.log(output);
